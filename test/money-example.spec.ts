a2de6d1e01877a127bdd11a8acf33f18372705b8
giuxtaposition <yg97.cs@gmail.com>
2021-10-12 14:07:09 +0200

    added test equality test

--- test/money-example.spec.ts
+++ test/money-example.spec.ts
@@ -5,8 +5,10 @@ import { Dollar } from "../src/index";
 [] $5 + 10 CHF = $10 if rate is 2:1
 [x] $5 * 2 = $10
 [] Make “amount” private
-[] Dollar side effects?
+[x] Dollar side effects?
 [] Money rounding?
+[] equals()
+[] hashCode()
 */
 
 describe("Money Example: ", function () {
@@ -16,5 +18,10 @@ describe("Money Example: ", function () {
     expect(product.amount).to.equal(10);
     product = five.times(3);
     expect(product.amount).to.equal(15);
+  });
+
+  it("test equality", function () {
+    expect(new Dollar(5)).to.equal(new Dollar(5));
+    expect(new Dollar(6)).not.to.equal(new Dollar(5));
   });
 });
